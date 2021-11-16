cccc45f9c8d910768921364df98832ec5ef3f3d9
honggeun <ghdrmsg@gmail.com>
2021-11-16 17:42:45 +0900

    eheh

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React master
+          Learn React master !!
         </a>
       </header>
     </div>
