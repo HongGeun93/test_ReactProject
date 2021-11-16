c92df49455cafdb9f093eec8ef51cfa8d53049b6
honggeun <ghdrmsg@gmail.com>
2021-11-16 17:45:47 +0900

    kwak branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React master !!
+          Learn React kwak branch
         </a>
       </header>
     </div>
