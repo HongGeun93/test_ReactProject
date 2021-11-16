24aced1b3b93f2065aa5e23e48b71f5ba71be574
honggeun <ghdrmsg@gmail.com>
2021-11-16 17:39:02 +0900

    master commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hong add kwak
+          Learn React master
         </a>
       </header>
     </div>
